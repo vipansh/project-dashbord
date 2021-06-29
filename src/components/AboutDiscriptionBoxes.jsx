ed09d47c9dc3d540130ea05f579e8899068b996c
Vipansh Thakur <vipansh@github.com>
2021-06-30 01:21:35 +0530

    ui size update

--- src/components/AboutDiscriptionBoxes.jsx
+++ src/components/AboutDiscriptionBoxes.jsx
@@ -28,7 +28,7 @@ const AboutDiscriptionBoxes = ({ onChangeFunction, data }) => {
             className="px-3 py-3 placeholder-blueGray-300 text-blueGray-600 relative bg-gray-300  rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full pr-10"
           />
           <div className="z-10 h-full leading-snug font-normal absolute text-center text-blueGray-300  bg-transparent rounded  items-center justify-end w-auto right-0 pr-3 py-3 flex text-xs">
-            <div className="inline-flex  flex-wrap max-w-w-10 justify-start items-center text-xs">
+            <div className="inline-flex  flex-wrap max-w-w-10 justify-start items-center text-xs" style={{fontSize:"0.45rem"}}>
               Quantity
             </div>
           </div>
@@ -46,7 +46,7 @@ const AboutDiscriptionBoxes = ({ onChangeFunction, data }) => {
             className="px-3 py-3 placeholder-blueGray-300 text-blueGray-600 relative bg-gray-300  rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full pr-10"
           />
           <div className="z-10 h-full leading-snug font-normal absolute text-center text-blueGray-300  bg-transparent rounded text-base items-center justify-end w-auto right-0 pr-3 py-3 flex">
-            <div className="inline-flex  flex-wrap max-w-w-10 justify-start items-center text-xs">
+            <div className="inline-flex  flex-wrap max-w-w-10 justify-start items-center text-xs " style={{fontSize:"0.45rem"}}>
               Rate
             </div>
           </div>
@@ -63,7 +63,7 @@ const AboutDiscriptionBoxes = ({ onChangeFunction, data }) => {
             className="px-3 py-3 placeholder-blueGray-300 text-blueGray-600 relative bg-gray-300  rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full pr-10"
           />
           <div className="z-10 h-full leading-snug font-normal absolute text-center text-blueGray-300  bg-transparent rounded text-base items-center justify-end w-auto right-0 pr-3 py-3 flex">
-            <div className="inline-flex  flex-wrap max-w-w-10 justify-start items-center text-xs">
+            <div className="inline-flex  flex-wrap max-w-w-10 justify-start items-center text-xs" style={{fontSize:"0.45rem"}}>
               Unit
             </div>
           </div>
